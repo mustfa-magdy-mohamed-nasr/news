4d20345b1e7a97f46fa07927a03708c41ff10507
mustfa-magdy-mohamed-nasr <Mostfammaaggddyy@gmail.com>
2024-06-18 08:57:27 +0300

    create fetchallartcle methed

--- lib/features/save/views/save_view.dart
+++ lib/features/save/views/save_view.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:news/features/save/views/widgets/custom_scrol_view_save_view_body.dart';
+import 'package:news/utl/cubits/artcl_cubit_cubit/artcl_cubit_cubit.dart';
 
 class SaveView extends StatelessWidget {
   const SaveView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const CustomScrollViewSaveViewBody();
+    return BlocProvider(
+      create: (context) => ArtclCubitCubit(),
+      child: const CustomScrollViewSaveViewBody(),
+    );
   }
 }
